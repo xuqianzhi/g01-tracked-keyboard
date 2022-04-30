345abe31134bf1f1e04375b835e77da8c878c91c
xuqianzhi <xuqianzhi1998@hotmail.com>
2022-04-30 17:21:27 -0400

    wpm and accuracy

--- tracked-keyboard/Assets/TextDisplayManagerBehavior.cs
+++ tracked-keyboard/Assets/TextDisplayManagerBehavior.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,33 +9,56 @@ using UnityEngine.UIElements;
 
 public class TextDisplayManagerBehavior : MonoBehaviour
 {
-    public GameObject cube;
     public InputField textInputField;
-    public Text indicatorTextField;
+    public Text primaryLabel;
+    public Text secondaryLabel;
+    
     private String[] sentence;
-
-    private int sentencePointer;
+    private int wordPointer;
+    private Boolean didSessionBegin;
+    
+    private Stopwatch timer;
+    private int correctWordCount;
 
     // Start is called before the first frame update
     void Start()
-    { 
-        // activate input field
-        textInputField.Select();
-        textInputField.ActivateInputField();
+    {
+        // deactivate input field until user begin session
+        textInputField.DeactivateInputField();
         
         // initialize variables
-        sentence = new[] { "i", "hope", "meta", "hire", "better", "software", "developer", "for", "the", "oculus", "team", "because", "the", "documentation", "literally", "sucks" };
-        sentencePointer = 0;
-        indicatorTextField.text = sentence[0];
+        didSessionBegin = false;
+        sentence = new[] { "hello", "world", "hello", "world", };
+        wordPointer = 0;
+        timer = new Stopwatch();
+        correctWordCount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!didSessionBegin)
+        {
+            UserBeginSession();
+            return;
+        }
         String userInput = textInputField.text;
+        userInput = userInput.Replace(" ", String.Empty);
         WordInputState state = GetWordInputState(userInput);
         HandleProceedToNextWord(state);
         UpdateInputFieldColor(state);
+    }
+
+    void UserBeginSession()
+    {
+        if (Event.current.Equals(Event.KeyboardEvent("return")))
+        {
+            textInputField.Select();
+            textInputField.ActivateInputField();
+            didSessionBegin = true;
+            primaryLabel.text = sentence[0];
+            timer.Start();
+        }
     }
 
     void UpdateInputFieldColor(WordInputState state)
@@ -53,42 +77,50 @@ public class TextDisplayManagerBehavior : MonoBehaviour
 
     void HandleProceedToNextWord(WordInputState state)
     {
-        if (state == WordInputState.SessionCompleted)
-            return;
+        if (state == WordInputState.SessionCompleted) return;
+        
         Event currEvent = Event.current;
         if (state != WordInputState.Empty &&
             (currEvent.Equals(Event.KeyboardEvent("return")) || currEvent.Equals(Event.KeyboardEvent("space"))))
         {
-            // update input field and indicator text field
-            textInputField.text = "";
-            sentencePointer += 1;
-            if (sentencePointer >= sentence.Length)
+            // user finished typing the current word
+            
+            // update accurate word count
+            if (state == WordInputState.Correct)
             {
-                indicatorTextField.text = "Session Completed!";
-                indicatorTextField.color = Color.green;
+                // update correct word count
+                correctWordCount += 1;
+            }
+            
+            // clear input field and increment word pointer
+            textInputField.text = "";
+            wordPointer += 1;
+            if (wordPointer >= sentence.Length)
+            {
+                // session completed
+                timer.Stop();
+                Double interval = timer.Elapsed.TotalSeconds;
+                Double wpm = Math.Round(correctWordCount / interval * 60, 2);
+                Double accuracy = Math.Round((Double)correctWordCount / sentence.Length, 4) * 100 ;
+                primaryLabel.text = "Session Complete!";
+                primaryLabel.color = Color.green;
+                secondaryLabel.text = $"wpm: {wpm}, accuracy: {accuracy}%";
             }
             else
             {
-                indicatorTextField.text = sentence[sentencePointer];
-            }
-            
-            // update UI, accuracy, and error rate according to input state
-            if (state == WordInputState.Correct)
-            {
-                // TODO: update accuracy
-            } else if (state == WordInputState.InCorrect || state == WordInputState.InProgress) {
-                // TODO: update error rate
+                // proceed to next word
+                primaryLabel.text = sentence[wordPointer];
             }
         }
     }
 
     WordInputState GetWordInputState(String userInput)
     {
-        if (sentencePointer >= sentence.Length)
+        if (wordPointer >= sentence.Length)
             return WordInputState.SessionCompleted;
         if (userInput.Length == 0)
             return WordInputState.Empty;
-        String target = sentence[sentencePointer];
+        String target = sentence[wordPointer];
         if (IsPrefix(target, userInput))
         {
             if (userInput.Length == target.Length)
